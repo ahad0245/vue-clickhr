e7b4f8638288685f7bbc0e65b7699826f9db7012
ahad0245 <abdulahad@i8is.com>
2025-05-30 21:22:35 +0500

    New updates

--- vue clickhr/src/App.vue
+++ vue clickhr/src/App.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <!-- Header/Logos -->
+    
+
+    <!-- Navigation Links -->
+    
+
+    <!-- Routed View -->
+    <router-view />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
@@ -26,5 +26,16 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+nav {
+  margin: 1rem 0;
+}
+nav a {
+  margin: 0 0.5rem;
+  text-decoration: none;
+  color: #42b983;
+}
+nav a.router-link-active {
+  font-weight: bold;
 }
 </style>
